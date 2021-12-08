dd26e3d43f5d8ef7a8644234c11199612616c9ea
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2021-12-08 17:56:29 +0200

    add html

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,38 +5,36 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const { addUser, getUser, deleteUser, getUsers } = require("./users");
 
 app.use(cors());
 
+// TOOD: add error handling
 io.on("connection", (socket) => {
-  socket.on("login", ({ name, room }, callback) => {
+  socket.on("login", ({ name, room }) => {
     const { user, error } = addUser(socket.id, name, room);
-    if (error) return callback(error);
-    socket.join(user.room);
-    socket.in(room).emit("notification", {
-      title: "Someone's here",
-      description: `${user.name} just entered the room`,
-    });
-    io.in(room).emit("users", getUsers(room));
-    callback();
+    if (user) {
+      console.log("User connected");
+      socket.join(room);
+      socket.to(room).emit("notification", `${name} just entered the room`);
+      io.in(room).emit("users", getUsers(room));
+    }
   });
 
-  socket.on("sendMessage", (message) => {
+  socket.on("message", (message) => {
     const user = getUser(socket.id);
-    io.to(user.room).emit("message", { user: user.name, text: message });
+    io.in(user.room).emit("message", { user: user.name, text: message });
   });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
     const user = deleteUser(socket.id);
     if (user) {
-      io.to(user.room).emit("notification", {
-        title: "Someone just left",
-        description: `${user.name} just left the room`,
-      });
-      io.to(user.room).emit("users", getUsers(user.room));
+      socket
+        .to(user.room)
+        .emit("notification", `${user.name} just left the room`);
+      io.in(user.room).emit("users", getUsers(user.room));
     }
   });
 });
