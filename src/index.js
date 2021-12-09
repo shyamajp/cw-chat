86128579fb7ef2e0126f576cd943dc3163222df6
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2021-12-09 10:12:47 +0200

    delete typescript

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import cors from "cors";
-import path from "path";
-import express from "express";
-
+const path = require("path");
+const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-
-const PORT = process.env.PORT || 3001;
 const { addUser, getUser, deleteUser, getUsers } = require("./users");
 
-app.use(cors());
+const PORT = 3000;
 
 // TOOD: add error handling
 io.on("connection", (socket) => {
+  console.log("User connected");
+
   socket.on("login", ({ name, room, frequency }) => {
     const { user, error } = addUser(socket.id, name, room, frequency);
     if (user) {
-      console.log("User connected");
       socket.join(room);
       socket.to(room).emit("notification", `${name} just entered the room`);
       io.in(room).emit("users", getUsers(room));
@@ -39,9 +36,7 @@ io.on("connection", (socket) => {
     console.log("User disconnected");
     const user = deleteUser(socket.id);
     if (user) {
-      socket
-        .to(user.room)
-        .emit("notification", `${user.name} just left the room`);
+      socket.to(user.room).emit("notification", `${user.name} just left the room`);
       io.in(user.room).emit("users", getUsers(user.room));
     }
   });
--- src/users.js
+++ src/users.js
@@ -1,9 +1,7 @@
 const users = [];
 
-export const addUser = (id, name, room, frequency) => {
-  const existingUser = users.find(
-    (user) => user.name.trim().toLowerCase() === name.trim().toLowerCase()
-  );
+const addUser = (id, name, room, frequency) => {
+  const existingUser = users.find((user) => user.name.trim().toLowerCase() === name.trim().toLowerCase());
 
   if (existingUser) return { error: "Username has already been taken" };
   if (!name && !room) return { error: "Username and room are required" };
@@ -15,14 +13,19 @@ export const addUser = (id, name, room, frequency) => {
   return { user };
 };
 
-export const getUser = (id) => {
+const getUser = (id) => {
   const user = users.find((user) => user.id == id);
   return user;
 };
 
-export const deleteUser = (id) => {
+const deleteUser = (id) => {
   const index = users.findIndex((user) => user.id === id);
   if (index !== -1) return users.splice(index, 1)[0];
 };
 
-export const getUsers = (room) => users.filter((user) => user.room === room);
+const getUsers = (room) => users.filter((user) => user.room === room);
+
+exports.addUser = addUser;
+exports.getUser = getUser;
+exports.deleteUser = deleteUser;
+exports.getUsers = getUsers;
