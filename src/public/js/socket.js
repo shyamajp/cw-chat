ce9649dbd91c32b70e2fcfc567c2f49ac7ae813a
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2021-12-09 18:57:01 +0200

    separate socket

--- src/public/js/socket.js
+++ src/public/js/socket.js
@@ -1,1 +1,56 @@
 export const socket = io();
+import { Beep, getOtherBeep, setOtherBeep } from "./beep.js";
+import { EventName } from "./types.js";
+import { getUser } from "./user.js";
+
+socket.on(EventName.Notification, (notification) => {
+  const ul = document.getElementById("user-notifications");
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(notification));
+  ul.appendChild(li);
+});
+
+socket.on(EventName.Users, (users) => {
+  const ul = document.getElementById("user-list");
+  ul.innerHTML = "";
+  users.forEach(({ name }) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(name));
+    ul.appendChild(li);
+  });
+});
+
+socket.on(EventName.Rooms, (rooms) => {
+  console.log(rooms);
+});
+
+socket.on(EventName.UserCount, (userCount) => {
+  console.log(userCount);
+});
+
+socket.on(EventName.Mode, (mode) => {
+  console.log(mode);
+});
+
+socket.on(EventName.Error, (error) => {
+  console.log(error);
+});
+
+socket.on(EventName.Message, (message) => {
+  const { id, user, text, frequency: othersFrequency } = message;
+
+  if (!getUser().transmit) {
+    if (text === "d") {
+      const beep = new Beep(othersFrequency);
+      setOtherBeep(id, beep);
+      beep.play();
+    } else if (text === "u") {
+      getOtherBeep(id).stop();
+    }
+  }
+
+  const ul = document.getElementById("messages");
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(`${user}: ${text}`));
+  ul.appendChild(li);
+});
--- src/public/script.js
+++ src/public/script.js
@@ -1,7 +1,3 @@
-import { Beep, getOtherBeep, setOtherBeep } from "./js/beep.js";
-import { EventName } from "./js/types.js";
-import { socket } from "./js/socket.js";
-import { getUser } from "./js/user.js";
 import { handleKeyUp, handleKeyDown } from "./js/keys.js";
 
 window.onload = () => {
@@ -13,56 +9,3 @@ window.onunload = () => {
   document.removeEventListener("keydown", handleKeyDown);
   document.removeEventListener("keyup", handleKeyUp);
 };
-
-/* SOCKET */
-socket.on(EventName.Notification, (notification) => {
-  const ul = document.getElementById("user-notifications");
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(notification));
-  ul.appendChild(li);
-});
-
-socket.on(EventName.Users, (users) => {
-  const ul = document.getElementById("user-list");
-  ul.innerHTML = "";
-  users.forEach(({ name }) => {
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(name));
-    ul.appendChild(li);
-  });
-});
-
-socket.on(EventName.Rooms, (rooms) => {
-  console.log(rooms);
-});
-
-socket.on(EventName.UserCount, (userCount) => {
-  console.log(userCount);
-});
-
-socket.on(EventName.Mode, (mode) => {
-  console.log(mode);
-});
-
-socket.on(EventName.Error, (error) => {
-  console.log(error);
-});
-
-socket.on(EventName.Message, (message) => {
-  const { id, user, text, frequency: othersFrequency } = message;
-
-  if (!getUser().transmit) {
-    if (text === "d") {
-      const beep = new Beep(othersFrequency);
-      setOtherBeep(id, beep);
-      beep.play();
-    } else if (text === "u") {
-      getOtherBeep(id).stop();
-    }
-  }
-
-  const ul = document.getElementById("messages");
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(`${user}: ${text}`));
-  ul.appendChild(li);
-});
