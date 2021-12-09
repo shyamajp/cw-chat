e3edd91af9e9a962f35c778a30712467ec2b7f5e
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2021-12-09 19:01:48 +0200

    add layout

--- src/public/components/login.js
+++ src/public/components/login.js
@@ -31,17 +31,21 @@ class Login extends HTMLElement {
   render() {
     this.innerHTML = `
     <form id="login-form">
-      <h2>Login</h2>
-      <label for="room">room</label>
-      <input id="room" name="room" />
-      <br />
-      <label for="name">name</label>
-      <input id="name" name="name" />
-      <br />
-      <label for="frequency">frequency</label>
-      <input type="range" min="440" max="1600" id="frequency" name="frequency" />
-      <br />
-      <button type="submit">login</button>
+      <div class='form-item'>
+        <label for="room">room</label>
+        <input id="room" name="room" />
+      </div>
+      <div class='form-item'>
+        <label for="name">name</label>
+        <input id="name" name="name" />
+      </div>
+      <div class='form-item'>
+        <label for="frequency">frequency</label>
+        <input type="range" min="440" max="1600" id="frequency" name="frequency" />
+      </div>
+      <div class='form-item'>
+        <button type="submit">login</button>
+      </div>
     </form>`;
   }
 }
